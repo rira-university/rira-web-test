ec58f8aa2d4e964566be08f3f7019c81e83701ad
Wonu Lee <wonwoolee24@gmail.com>
2022-01-23 17:22:08 +0900

    Add empty root page for redirection

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,7 +1,3 @@
 import {render} from '@testing-library/react'
-import Home from '@/pages/index.page'
 
-it('renders homepage unchanged', () => {
-  const {container} = render(<Home />)
-  expect(container).toMatchSnapshot()
-})
+it('renders homepage unchanged', () => {})
--- pages/index.page.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <Head>
-      <meta httpEquiv='refresh' content='0; url=en' />
-    </Head>
-  )
-}
