f0a7d2f76becf63f239030b6edcaa55b036ee560
wonu <wonwoolee24@gmail.com>
2022-01-23 08:25:42 +0000

    Deploying to gh-pages from @ rira-university/rira-web-test@ec58f8aa2d4e964566be08f3f7019c81e83701ad 🚀

---
+++ _next/static/3XvNepTUZcBYCVFrlmrGM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/[langCode]":["static/chunks/pages/[langCode]-baa353b1af1c05ad.js"],sortedPages:["/_app","/_error","/[langCode]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
