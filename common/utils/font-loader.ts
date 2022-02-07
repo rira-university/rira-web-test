ff9631a46814dcf7a8f6b843e4da7ea6df188cac
Wonu Lee <wonwoolee24@gmail.com>
2022-02-08 02:26:32 +0900

    Change font name

--- common/utils/font-loader.ts
+++ common/utils/font-loader.ts
@@ -1,5 +1,5 @@
 export const baloo2 = `'Baloo 2'`
-export const balooDa2 = `'BalooDa 2'`
+export const balooDa2 = `'Baloo Da 2'`
 export const notoSansJp = `'Noto Sans JP'`
 export const oneMobileRegular = `'ONE-Mobile-Regular'`
 export const oneMobilePop = `'ONE-Mobile-POP'`
